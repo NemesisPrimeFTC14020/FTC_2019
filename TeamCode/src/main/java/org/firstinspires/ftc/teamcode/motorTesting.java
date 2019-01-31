1c2616259bbe7f8f48a6492f86c586abe8bc9d61
Jeremy Murray <21murrayj@rvilleschools.org>
2019-01-30 19:57:53 -0500

    Tensorflow Rework, scanning with edge pixel fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTesting.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="AVEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEK", group="Linear Opmode")
-public class Aveeeeeeek extends LinearOpMode {
+@TeleOp(name="Motor Testing", group="Linear Opmode")
+public class motorTesting extends LinearOpMode {
     public Variables Variables = new Variables();
     public Methods Methods = new Methods();
     public Hardware Hardware = new Hardware();
@@ -14,17 +14,17 @@ public class Aveeeeeeek extends LinearOpMode {
         Hardware.hardwareMap(this);
         waitForStart();
         Variables.runtime.reset();
-        DcMotor motorboi = null;
-        motorboi = hardwareMap.get(DcMotor.class, "motorboi");
-        motorboi.setDirection(DcMotor.Direction.FORWARD);
+        DcMotor motor = null;
+        motor = hardwareMap.get(DcMotor.class, "motorboi");
+        motor.setDirection(DcMotor.Direction.FORWARD);
        // double speedlimiter = 0;
         while (opModeIsActive()) {
            if(gamepad1.a){
-               motorboi.setPower(1);
+               motor.setPower(1);
                telemetry.addLine("Forward");
            }
            else if(gamepad1.b){
-               motorboi.setPower(-1);
+               motor.setPower(-1);
                telemetry.addLine("Backward");
            }
         }
