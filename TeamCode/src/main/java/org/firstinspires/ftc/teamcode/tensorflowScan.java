d920feabf34e3453eb639e1a893f58d0ee1791d5
metachansite <metachan@protonmail.com>
2019-02-24 12:50:03 -0500

    made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tensorflowScan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tensorflowScan.java
@@ -5,9 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="EXAMPLE", group="Linear Opmode")
 public class tensorflowScan extends LinearOpMode {
     public Methods methods = new Methods();
-    methods.Hardware.initHardware(this);
     public void runOpMode() {
         methods.Hardware.initHardware(this);
         waitForStart();
+        while(true){
+            methods.goldAngle(this);
+            telemetry.update();
+        }
+
+
     }
 }
